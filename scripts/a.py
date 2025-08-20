f802a28b0880e4df06d355faae5a646032b49037
Sadia <sadia@Sadias-Air.lan>
2025-08-20 21:17:51 +0530

    Correct renko plotted

--- scripts/a.py
+++ scripts/a.py
@@ -1,9 +1,18 @@
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../stock_helper_lib')))
+
 from stockhelper.core import get_stock_data
 from stockhelper.indicators.renko import get_renko_data
-from stockhelper.plot import plot_ohlc_and_renko
+from stockhelper.plot import plot_renko_only, plot_renko_matplotlib, plot_renko_equal_time_finplot
 
-df = get_stock_data("AAPL", "2023-01-01", "2023-07-01", interval="1d")
+
+import datetime
+end_date = datetime.date.today()
+start_date = end_date - datetime.timedelta(days=365)
+df = get_stock_data("RELIANCE.NS", str(start_date), str(end_date), interval="1d")
+
 
 if not df.empty:
 	dup_count = df.index.duplicated().sum()
@@ -13,7 +22,7 @@ if not df.empty:
 	else:
 		print("No duplicate indices in df.")
 
-	renko_df = get_renko_data(df, brick_pct=0.01)
+	renko_df = get_renko_data(df, brick_pct=0.005)
 	if not renko_df.empty:
 		renko_dup_count = renko_df.index.duplicated().sum()
 		if renko_dup_count > 0:
@@ -21,7 +30,9 @@ if not df.empty:
 			renko_df = renko_df[~renko_df.index.duplicated(keep='first')]
 		else:
 			print("No duplicate indices in renko_df.")
-		plot_ohlc_and_renko(df, renko_df)
+	# plot_renko_only(df, renko_df)
+		plot_renko_matplotlib(renko_df)
+		plot_renko_equal_time_finplot(renko_df)
 	else:
 		print("Renko data is empty. Cannot plot.")
 else:
